ff131198379ae5c9aa9856ecc5bbc20742d6851f
Natalia Janik <nataliia.janik@gmail.com>
2024-04-24 10:33:21 +0200

    fix

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from typing import Dict, Any
 
 
-def waluta_dict_na_str():
-    
-    dictionary: Dict[str, Any] = {"galeon": None, "sykl": None, "knut": None}
-    
-    galeony = input("Podaj ilosc galeonow: ")
-    
-    sykle = input("Podaj ilosc sykli: ")
-    
-    knuty = input("Podaj ilosc knutow: ")
-    
-    dictionary["galeon"] = galeony
-    
-    dictionary["sykl"] = sykle
-    
-    dictionary["knut"] = knuty
+def waluta_dict_na_str(dictionary):
     
     sentence = ""
     
