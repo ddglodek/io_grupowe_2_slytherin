40a2f53ecab2f19f6986b29ce121e6bb875e28e5
Andrzej Jerzykiewicz <83587@student.ue.poznan.pl>
2024-04-24 10:30:43 +0200

    poprawienie zadania

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import time
 import random
 
-def wyslij_sowe():
-    adresat = input("Podaj adresata: ")
-    tresc_listu = input("Wpisz treść listu: ")
-    print(f"Wysyłam sowę do {adresat} z treścią: {tresc_listu}")
-    time.sleep(1)  
+def wyslij_sowe(adresat, tresc_listu):
+    # adresat = input("Podaj adresata: ")
+    # tresc_listu = input("Wpisz treść listu: ")
+   
     powodzenie = random.choices([True, False], weights=[85, 15], k=1)[0]
+    if powodzenie:
+        time.sleep(1)  
+        print(f"Wysyłam sowę do {adresat} z treścią: {tresc_listu}")
     return powodzenie
+wyslij_sowe("Hagrid", "Wpadamy na herbate")
 
-wyslij_sowe()
+# i = 100
+# while i > 0:
+#     wyslij_sowe("Hagrid", "Wpadamy na herbate")
+#     i -= 1
