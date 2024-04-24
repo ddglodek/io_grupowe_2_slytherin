9631a3e206715c75fdffc354963a7fe3a098cf29
Natalia Janik <nataliia.janik@gmail.com>
2024-04-24 10:21:58 +0200

    function z5

--- main.py
+++ main.py
@@ -18,6 +18,25 @@ def waluta_dict_na_str():
     dictionary["knut"] = knuty
     
     print(dictionary)
+    
+    sentence = ""
+    
+    tableKeys = list(dictionary.keys())
+    
+    tableVals = []
+    for value in dictionary.values():
+        tableVals.append(int(value))
+    
+    for i in range(len(tableVals)):
+        if tableVals[i] != 0:
+            if i == 0 or i ==2:
+                sentence += str(tableVals[i]) + " " + tableKeys[i] + "y "
+            else:
+                sentence += str(tableVals[i]) + " " + tableKeys[i] + "e "
+        else:
+            sentence += ""
+
+    print(sentence)
 
 waluta_dict_na_str()
 
