06bb77b7e994ea3b28c866ebf6dd303813966ec2
Andrzej Jerzykiewicz <83587@student.ue.poznan.pl>
2024-05-15 14:26:58 +0200

    test added

--- test_wyslij_sowe.py
+++ test_wyslij_sowe.py
@@ -5,13 +5,14 @@ def test_wyslij_sowe_from_file():
       # type: ignore # Import the function from main.py
     
     sukcesy = 0
-    total_attempts = 100
+    total_attempts = 10
     
-    while total_attempts > 0):
+    for _ in range(total_attempts):
         if wyslij_sowe("Harry Potter", "Nie zapomnij o miotle!"):
             sukcesy += 1
-        total_attempts -= 1
+
     print(f"Liczba udanych wysyłek: {sukcesy} z {total_attempts}")
+    return sukcesy/total_attempts 
     
     
-test_wyslij_sowe_from_file()
+assert(0.8 <= test_wyslij_sowe_from_file() <= 0.9)
