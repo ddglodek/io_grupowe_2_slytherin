bf8e30764d6c3981f01a2c3777010705dbe2cf08
NataliaRat <162417134+NataliaRat@users.noreply.github.com>
2024-05-15 11:11:54 +0200

    added zad 6 (#1)
    * dodanie zad 6
    
    * poprawienie zad 6
    
    ---------
    
    Co-authored-by: NataliaRat <nataliarataj@vp.pl>

--- main.py
+++ main.py
@@ -107,3 +107,26 @@ def waluta_dict_na_str(dictionary):
             sentence += ""
 
     return sentence    
+  
+def waluta_str_na_dict(input_str):
+    
+    currency_dict = {
+        "galeon": 0,
+        "sykl": 0,
+        "knut": 0
+    }
+
+    tokens = input_str.split()
+
+    for i in range(0, len(tokens), 2):
+        value = int(tokens[i])
+        key = tokens[i + 1]
+
+        if key.startswith('g'):
+            currency_dict["galeon"] = value
+        elif key.startswith('s'):
+            currency_dict["sykl"] = value
+        elif key.startswith('k'):
+            currency_dict["knut"] = value
+
+    return currency_dict
