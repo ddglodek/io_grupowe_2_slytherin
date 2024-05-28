5f624211fe0a922420f1ef570e6a649432d39a96
Nikola Hrabska <83529@student.ue.poznan.pl>
2024-05-28 22:03:54 +0200

    final ver. of tests

--- testzad4.py
+++ testzad4.py
@@ -5,9 +5,9 @@ from main import wybierz_sowe_zwroc_koszt
 
 
 assert(wybierz_sowe_zwroc_koszt(True, "lokalna", "list", "wyjec") == {'galeon': 0, 'sykl': 0, 'knut': 13})
-print(wybierz_sowe_zwroc_koszt(True, "dalekobiezna", "list", None))
-print(wybierz_sowe_zwroc_koszt(False, "krajowa", "paczka", None))
+assert(wybierz_sowe_zwroc_koszt(True, "dalekobiezna", "list", None) == {'galeon': 0, 'sykl': 1, 'knut': 6})
+assert(wybierz_sowe_zwroc_koszt(False, "krajowa", "paczka", None) == {'galeon': 0, 'sykl': 1, 'knut': 2})
 #testy dla przypadków niepoprawnych
-print(wybierz_sowe_zwroc_koszt(True, "lokalna", "list", "")) # nieprawidłowy typ specjalnej przesyłki
-print(wybierz_sowe_zwroc_koszt(True, "lokalna", "", "wyjec")) # nieprawidłowy typ przesyłki
-print(wybierz_sowe_zwroc_koszt(True, "", "list", "wyjec")) # brak podanej odległości
+assert(wybierz_sowe_zwroc_koszt(True, "lokalna", "list", "") == {'galeon': 0, 'sykl': 0, 'knut': 9})# nieprawidłowy typ specjalnej przesyłki
+assert(wybierz_sowe_zwroc_koszt(True, "lokalna", "", "wyjec") == {'galeon': 0, 'sykl': 0, 'knut': 11})# nieprawidłowy typ przesyłki
+assert(wybierz_sowe_zwroc_koszt(True, "", "list", "wyjec") == {'galeon': 0, 'sykl': 0, 'knut': 11})# brak podanej odległości
